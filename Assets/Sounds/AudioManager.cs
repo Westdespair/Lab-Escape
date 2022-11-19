116eba0bcd1cf6e6629dca34ac60e1e244226f74
alegro91 <alejandro_mtg@hotmail.com>
2022-11-19 20:08:05 +0100

    working complete audio management

--- Assets/Sounds/AudioManager.cs
+++ Assets/Sounds/AudioManager.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
 
+    [SerializeField] AudioMixer mixer;
     [SerializeField] AudioSource actionSource;
     [SerializeField] List<AudioClip> actionClips = new List<AudioClip>();
+
+    public const string MUSIC_KEY = "musicVolume";
+    public const string SFX_KEY = "sfxVolume";
 
     void Awake()
     {
@@ -22,13 +27,22 @@ public class AudioManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        Load();
     }
 
-
-    public void ActionSFX()
+    public void ActionSFX(int value)
     {
-    AudioClip clip = actionClips[Random.Range(0, actionClips.Count)];
+    AudioClip clip = actionClips[value];
     actionSource.PlayOneShot(clip);
+    } 
+
+    void Load()
+    {
+        float musicVolume = PlayerPrefs.GetFloat(MUSIC_KEY, 0.5f);
+        float sfxVolume = PlayerPrefs.GetFloat(SFX_KEY, 0.5f);
+
+        mixer.SetFloat(VolumeSettings.MIXER_MUSIC, Mathf.Log10(musicVolume)*20);
+        mixer.SetFloat(VolumeSettings.MIXER_SFX, Mathf.Log10(sfxVolume) * 20);
     }
 
 }
--- Assets/Sounds/VolumeSettings.cs
+++ Assets/Sounds/VolumeSettings.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
-
 public class VolumeSettings : MonoBehaviour
 {
     [SerializeField] AudioMixer mixer;
     [SerializeField] Slider musicSlider;
     [SerializeField] Slider sfxSlider;
 
-    const string MIXER_MUSIC = "MusicVolume";
-    const string MIXER_SFX = "SFXVolume";
+    public const string MIXER_MUSIC = "MusicVolume";
+    public const string MIXER_SFX = "SFXVolume";
 
 
     void Awake()
@@ -21,41 +20,31 @@ public class VolumeSettings : MonoBehaviour
         sfxSlider.onValueChanged.AddListener(SetSFXVolume);
     }
 
-    void SetMusicVolume(float value)
-    {
-        mixer.SetFloat(MIXER_MUSIC, Mathf.Log10(value)*20);
-        Save();
-    }
-
-    void SetSFXVolume(float value)
-    {
-        mixer.SetFloat(MIXER_SFX, Mathf.Log10(value)*20);
-        Save();
-    }
-
     void Start()
     {
-        if (!PlayerPrefs.HasKey("MusicVolume"))
-        {
-            PlayerPrefs.SetFloat("MusicVolume", 0.5f);
-            Load();
-        }
-
-        else
-        {
-            Load();
-        }
+        musicSlider.value = PlayerPrefs.GetFloat(AudioManager.MUSIC_KEY, 0.5f);
+        sfxSlider.value = PlayerPrefs.GetFloat(AudioManager.SFX_KEY, 0.5f);
     }
 
-    private void Load()
+
+    void OnDisable() 
     {
-        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        PlayerPrefs.SetFloat(AudioManager.MUSIC_KEY, musicSlider.value);
+        PlayerPrefs.SetFloat(AudioManager.SFX_KEY, sfxSlider.value);
     }
 
-    private void Save()
+    public void SetMusicVolume(float valueMusic)
     {
-        PlayerPrefs.SetFloat("MusicVolume", musicSlider.value);
-        PlayerPrefs.SetFloat("SFXVolume", sfxSlider.value);
+        mixer.SetFloat(MIXER_MUSIC, Mathf.Log10(valueMusic) *20);
 
     }
+
+    public void SetSFXVolume(float valueSFX)
+    {
+        mixer.SetFloat(MIXER_SFX, Mathf.Log10(valueSFX) *20);
+        //AudioManager.instance.ActionSFX(0);
+
+    }
+
+
 }
