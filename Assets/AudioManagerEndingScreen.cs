ed15499728620a83166617aa8c72ee6a96ab65c4
alegro91 <alegro91@users.noreply.github.com>
2022-11-28 13:59:05 +0100

    Fixed switching themes from ending scene
    Fixed switching themes from ending theme to main menu theme when you transition

---
+++ Assets/AudioManagerEndingScreen.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManagerEndingScreen : MonoBehaviour
+{
+    public static AudioManagerEndingScreen instance;
+
+    [SerializeField] AudioMixer mixer;
+    [SerializeField] AudioSource actionSource;
+    [SerializeField] List<AudioClip> actionClips = new List<AudioClip>();
+
+    public const string MUSIC_KEY = "musicVolume";
+    public const string SFX_KEY = "sfxVolume";
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            
+        }
+        Load();
+    }
+
+    public void ActionSFX(int value)
+    {
+    AudioClip clip = actionClips[value];
+    actionSource.PlayOneShot(clip);
+    } 
+
+    void Load()
+    {
+        float musicVolume = PlayerPrefs.GetFloat(MUSIC_KEY, 0.5f);
+        float sfxVolume = PlayerPrefs.GetFloat(SFX_KEY, 0.5f);
+
+        mixer.SetFloat(VolumeSettings.MIXER_MUSIC, Mathf.Log10(musicVolume)*20);
+        mixer.SetFloat(VolumeSettings.MIXER_SFX, Mathf.Log10(sfxVolume) * 20);
+    }
+
+}
